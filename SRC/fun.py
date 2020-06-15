85a8d5236fb918a6883b808429cf14d86e612949
Antonio Ortiz <antonio.ortizms@me.com>
2020-06-15 23:39:45 +0200

    Adding funtions2

--- SRC/fun.py
+++ SRC/fun.py
@@ -2,3 +2,9 @@
 
 def drop_null(table,subset):
     table.dropna(subset=[f"{subset}"],inplace=True)
+
+def standardize_data(df,column,old,new):
+    Valid_colums[column] = Valid_colums[column].replace(old, new)
+
+def c_data(column):
+     print(Valid_colums[f"{column}"].value_counts())
